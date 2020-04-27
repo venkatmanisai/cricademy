d0d100c35f6d732cb4509e33a7e5148134f0354e
PV <venkat.sai377@gmail.com>
2020-04-27 23:44:29 +0530

    added models to learn and users

--- learn/models.py
+++ learn/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.course_name
+
+
+class Skillcategory(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    skillcategory_name = models.CharField(max_length=100)
+    priority = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.skillcategory_name
+
+
+class Skill(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    category = models.ForeignKey(Skillcategory, on_delete=models.CASCADE)
+    skill_name = models.CharField(max_length=100)
+    skill_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.skill_name
+
+
+class Videocategory(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    videocategory_name = models.CharField(max_length=100)
+    priority = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.videocategory_name
+
+
+class Video(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    skill_category = models.ForeignKey(Skillcategory, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    category = models.ForeignKey(Videocategory, on_delete=models.CASCADE)
+    video_name = models.CharField(max_length=100)
+    video = models.FileField(null=True, blank=True)
+
+    def __str__(self):
+        return self.video_name
+
+
+class Challenge(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    challenge_name = models.CharField(max_length=100)
+    priority = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.challenge_name
+
+
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
+    phone_1 = models.CharField(max_length=10)
+    phone_2 = models.CharField(max_length=10, null=True)
+    dob = models.DateField(null=True)
+    institute = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=250, null=True)
+    pincode = models.IntegerField(default=0, null=True)
+    discount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.full_name
+
+
