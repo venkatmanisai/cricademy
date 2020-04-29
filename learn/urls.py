95560d608d4b896ace5c31fc8dfe0b1347b58b4f
umamaheshwar-g <umamaheshwarcool@gmail.com>
2020-04-30 00:05:06 +0530

    small changes

--- learn/urls.py
+++ learn/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'learn'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name=app_name),
 ]
--- learn/views.py
+++ learn/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Home page")
+    return render(request,'learn/learn.html')
 # Create your views here.
